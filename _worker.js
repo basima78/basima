1d4c7eee71b4d9b6e5e75f946a6704eed7218198
basima78 <168558912+basima78@users.noreply.github.com>
2024-12-10 13:01:44 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b279446c-d637-4ba6-959c-cf0af1960e2f';
 
-const พร็อกซีไอพีs = ['138.2.105.213'];
+const พร็อกซีไอพีs = ['34.143.192.60'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
